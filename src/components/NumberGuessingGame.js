bea4972be7db7bb0649cae847ea5b95fdd046c46
Wessel Konstantinov <wes_kon1@hotmail.com>
2021-03-26 12:25:21 +0100

    Create parent component for the game

---
+++ src/components/NumberGuessingGame.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class NumberGuessingGame extends React.Component {
+    render() {
+        return (
+            <h1>Number Guessing Game</h1>
+        )
+    }
+}
+
+export default NumberGuessingGame;
